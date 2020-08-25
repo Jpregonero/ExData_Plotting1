b0304d44044ff88a839b2c332aaad4a373ba277a
JPrego <Jonathan.Pregonero@gmail.com>
2020-08-25 17:20:42 -0500

    Final version

---
+++ plot1.R
@@ -0,0 +1,29 @@
+library(data.table)
+library(sqldf)
+library(lubridate)
+
+##Getting required data using a query
+
+data <- read.csv.sql("household_power_consumption.txt",
+  sql = "select* from file where Date = '1/2/2007' or Date = '2/2/2007' ",
+  sep = ";")
+
+
+## Fixes date an time variables to right format:
+data$Date <- dmy(data$Date)
+data$Time <- hms(data$Time)
+
+
+length(complete.cases(data))  ## 2880 complete cases equals nrows(data)
+
+par(bg=NA)
+
+## Generates the histogram with color, title and labels:
+hist(data$Global_active_power,col="orangered2",
+      xlab="Global Active Power (kilowats)",   
+      main="Global Active Power", bg = NA
+     )
+
+## Creates png file with default size = 480 x 480
+dev.copy(png, file = "plot1.png")
+dev.off()
